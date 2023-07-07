6b2e52b4177098374d86949bd7b58be458df6a62
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-07-07 10:14:42 +0200

    add DBRef

--- backend/src/main/java/de/iav/backend/model/Teacher.java
+++ backend/src/main/java/de/iav/backend/model/Teacher.java
@@ -1,14 +1,22 @@
 package de.iav.backend.model;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 import java.util.List;
 
 
+@Document(collection = "teachers")
 public record Teacher(
+        @MongoId
+        String teacherId,
         String loginName,
         String firstName,
         String lastName,
         String email,
-        List<Course>courseList
+        @DBRef
+        List<Course> courseList
 ) {
 
 }
