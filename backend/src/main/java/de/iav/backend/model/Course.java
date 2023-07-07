7f3832d50cb88b0998d3b04e4d5c85e473add256
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-07-07 14:36:25 +0200

    add DBRef

--- backend/src/main/java/de/iav/backend/model/Course.java
+++ backend/src/main/java/de/iav/backend/model/Course.java
@@ -12,9 +12,9 @@ public record Course(
         @MongoId
         String courseId,
         String courseName,
-        @DBRef
+        @DBRef(db = students)
         List<Student> students,
-        @DBRef
+        @DBRef(db = teachers)
         Teacher teacher
 ) {
 
