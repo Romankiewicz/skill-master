0ee386728a7ece7566aa3c3208c5497bc0b115f8
Ziyang Song <>
2023-07-07 16:25:31 +0200

    Fix usage of annotation @DBRef, make stuff compiles

--- backend/src/main/java/de/iav/backend/model/Student.java
+++ backend/src/main/java/de/iav/backend/model/Student.java
@@ -15,7 +15,7 @@ public record Student(
         String firstName,
         String lastName,
         String email,
-        @DBRef(db = courses)
+        @DBRef(db = "courses")
         List<Course> courseList
 
 ) {
