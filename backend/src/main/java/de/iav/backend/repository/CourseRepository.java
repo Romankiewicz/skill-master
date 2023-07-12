d2429ae758a31ae7929af1b234da2286a70602be
Tobias Hengelbrock <tobias.hengelbrock@iav.de>
2023-07-12 11:03:05 +0200

    Add Annotation Repository

--- backend/src/main/java/de/iav/backend/repository/CourseRepository.java
+++ backend/src/main/java/de/iav/backend/repository/CourseRepository.java
@@ -2,7 +2,9 @@ package de.iav.backend.repository;
 
 import de.iav.backend.model.Course;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends MongoRepository<Course, String> {
     Course findCourseByCourseNameEquals(String courseName);
 }
