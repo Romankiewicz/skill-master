c799c9e4e58811fa1ad3f151e8076f1b837d6ab6
Ziyang Song <>
2023-07-14 17:05:53 +0200

    Cleanup: Change name of application; use password field in registration view

--- frontend/src/main/java/de/iav/frontend/SkillMasterApplication.java
+++ frontend/src/main/java/de/iav/frontend/SkillMasterApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SkillMasterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/de/iav/frontend/fxml/login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SkillMasterApplication.class.getResource("/de/iav/frontend/fxml/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 615, 337);
         stage.setTitle("Hello!");
         stage.setScene(scene);
