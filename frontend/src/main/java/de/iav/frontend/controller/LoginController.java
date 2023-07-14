cbd98e81242380d88e924826a88abe886c31e6a5
Ziyang Song <>
2023-07-14 15:34:07 +0200

    Add event for combo box

--- frontend/src/main/java/de/iav/frontend/controller/LoginController.java
+++ frontend/src/main/java/de/iav/frontend/controller/LoginController.java
@@ -1,4 +1,31 @@
 package de.iav.frontend.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField userName_TF;
+
+    @FXML
+    private PasswordField password_TF;
+
+    @FXML
+    private ComboBox userTypeSelector_CB;
+
+    public void initialize() {
+
+        System.out.println(userTypeSelector_CB.getValue());
+
+        userTypeSelector_CB.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) ->
+                {
+                    System.out.println(newValue);
+                }
+        );
+    }
+
 }
