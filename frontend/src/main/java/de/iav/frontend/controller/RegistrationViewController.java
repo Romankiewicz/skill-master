7ded0bfc180cf8d22f2a9d2788603ad081400171
Tobias Hengelbrock <tobias.hengelbrock@iav.de>
2023-07-14 15:34:50 +0200

    add fxml references

---
+++ frontend/src/main/java/de/iav/frontend/controller/RegistrationViewController.java
@@ -0,0 +1,72 @@
+package de.iav.frontend.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class RegistrationViewController {
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+    @FXML
+    private TextField firstNameTextField;
+    @FXML
+    private TextField lastNameTextField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private CheckBox gdprCheckbox;
+
+    @FXML
+    private Button registerButton;
+    public void initialize(){
+        registerButton.setDisable(true);
+    }
+
+    @FXML
+    public void onRegisterButtonClick_switchToNextView () throws IOException {
+        if(isEveryTextFieldValid()){
+            errorLabel.setText("Alles palletti");
+        }
+    }
+
+    private boolean isEveryTextFieldValid() {
+        if (userNameTextField.getText() == null || userNameTextField.getText().isEmpty()) {
+            errorLabel.setText("Bitte gib einen Username ein");
+            return false;
+        } else if (passwordTextField.getText() == null || passwordTextField.getText().isEmpty()) {
+            errorLabel.setText("Bitte gib einen Passwort ein");
+            return false;
+        } else if (firstNameTextField.getText() == null || firstNameTextField.getText().isEmpty()) {
+            errorLabel.setText("Bitte gib einen Vornamen ein");
+            return false;
+        } else if (lastNameTextField.getText() == null || lastNameTextField.getText().isEmpty()) {
+            errorLabel.setText("Bitte gib einen Nachnamen ein");
+            return false;
+        } else if (emailTextField.getText() == null || emailTextField.getText().isEmpty()) {
+            errorLabel.setText("Bitte gib eine E-Mail-Adresse ein");
+            return false;
+        } else if (!gdprCheckbox.isSelected()) {
+            errorLabel.setText("Bitte akzeptiere die Datenschutzerklärung");
+            return false;
+        } else {
+            errorLabel.setText("");
+            return true;
+        }
+    }
+
+    @FXML
+    public void onCancelButtonClick_switchToLogin(){
+        System.out.println("Jetzt wechsle ich zur Startseite");
+    }
+
+}
