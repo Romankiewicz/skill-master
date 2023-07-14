97a24f80ebd3fc4e63997d3b6b87bbace2179440
Ziyang Song <>
2023-07-14 15:38:54 +0200

    Add feature: switch scene to registration view on clicking the registration button

--- frontend/src/main/java/de/iav/frontend/controller/LoginController.java
+++ frontend/src/main/java/de/iav/frontend/controller/LoginController.java
@@ -1,9 +1,16 @@
 package de.iav.frontend.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -26,6 +33,24 @@ public class LoginController {
                     System.out.println(newValue);
                 }
         );
+    }
+
+    @FXML
+    public void onClick_switchToRegisterView(ActionEvent event) throws IOException {
+
+        // load layout of scene
+        FXMLLoader loaderRegView = new FXMLLoader(getClass().getResource("/de/iav/frontend/fxml/registration-view.fxml"));
+
+        // set scene object which should display the content in scene2
+        Scene sceneRegView = new Scene(loaderRegView.load());
+
+        // set stage which should be shown (newly) on click
+        Stage stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+
+        // set scene2 to stage and show it
+        stage.setScene(sceneRegView);
+        stage.show();
+
     }
 
 }
