4c799e6ffc82c0554b8b7fd9991abd099c9a8965
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-07-14 17:39:57 +0200

    add student-view service

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module de.iav.frontend {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.net.http;
+    requires com.fasterxml.jackson.databind;
+
+
     opens de.iav.frontend to javafx.fxml;
     exports de.iav.frontend;
+
+    opens de.iav.frontend.model to com.fasterxml.jackson.databind;
+    exports de.iav.frontend.model;
 }
