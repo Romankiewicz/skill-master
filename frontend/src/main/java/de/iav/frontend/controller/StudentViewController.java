ed12fd5d08d3c26d443cda1bc1ef1316345404b9
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-07-15 07:38:30 +0200

    add listView add switchToLoginView

--- frontend/src/main/java/de/iav/frontend/controller/StudentViewController.java
+++ frontend/src/main/java/de/iav/frontend/controller/StudentViewController.java
@@ -1,10 +1,16 @@
 package de.iav.frontend.controller;
-import de.iav.frontend.model.CourseDTO_RequestBody;
+
 import de.iav.frontend.model.Student;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+
+import java.io.IOException;
+import java.util.List;
+
+import static java.awt.SystemColor.text;
 
 
 public class StudentViewController {
@@ -14,11 +20,35 @@ public class StudentViewController {
     @FXML
     private Button addCourse;
     @FXML
-    private ComboBox<CourseDTO_RequestBody> courseListe;
+    private ComboBox<String> courseListe;
     @FXML
-    private ListView<Student>courseListOfStudent;
+    private ListView<Student> listView;
+
+    @FXML
+    private final StudentViewService studentViewService = StudentViewService.getInstance();
+    @FXML
+    private final SceneSwitchService sceneSwitchService = SceneSwitchService.getInstance();
 
 
+    public void initialize() {
+        List<String> allCourses = studnetViewService.getAllCourses();
+        listView.getItems().add(allCourses);
+        listView.getSelectionModel()
+                .selectedItemProperty()
+                .addListener(
+                        (observableValue, course, t1) -> {
+                            text.setText(listView.getSelectionModel().getSelectedItems()course());
+                        }
+                );
+    }
 
+    @FXML
+    public void onLogoutButtonClick_switchToLoginView(ActionEvent event) throws IOException {
+        SceneSwitchService.getInstance().switchToLoginView(event);
+    }
 
+    @FXML
+    public void addCourseToStudentCourseList(ActionEvent event) throws IOException {
+
+    }
 }
