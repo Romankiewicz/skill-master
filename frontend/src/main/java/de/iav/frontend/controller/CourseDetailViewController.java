bac96fcfb2e778db5e877233109c5ebca2e543a0
Tobias Hengelbrock <tobias.hengelbrock@iav.de>
2023-07-21 15:17:41 +0200

    Add course detail controller and injection of fxml fields

--- frontend/src/main/java/de/iav/frontend/controller/CourseDetailViewController.java
+++ frontend/src/main/java/de/iav/frontend/controller/CourseDetailViewController.java
@@ -1,5 +1,37 @@
 package de.iav.frontend.controller;
 
-public class CourseDetailViewController {
+import de.iav.frontend.service.SceneSwitchService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
+public class CourseDetailViewController {
+    @FXML
+    private TextField courseNameTextField;
+
+    @FXML
+    private TextField courseContentTextField;
+
+    @FXML
+    private ListView studentsOfCourseListView;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    public void onCancelButtonClickSwitchToTeacherScene(ActionEvent event) throws IOException {
+        SceneSwitchService.getInstance().switchToLoginView(event);
+    }
+
+    @FXML
+    public void onSubmitButtonClickSwitchToTeacherSceneAndSaveCourse(ActionEvent event) throws IOException{
+        SceneSwitchService.getInstance().switchToLoginView(event);
+    }
 }
