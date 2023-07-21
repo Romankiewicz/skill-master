a307a6dee366a9970ccebac8647b0a3260a787eb
Tobias Hengelbrock <tobias.hengelbrock@iav.de>
2023-07-21 10:11:48 +0200

    add injection field for fxml

--- frontend/src/main/java/de/iav/frontend/controller/TeacherViewController.java
+++ frontend/src/main/java/de/iav/frontend/controller/TeacherViewController.java
@@ -1,4 +1,30 @@
 package de.iav.frontend.controller;
 
+import de.iav.frontend.service.SceneSwitchService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+import java.io.IOException;
+
 public class TeacherViewController {
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button newCourseButton;
+
+    @FXML
+    private ListView coursesOfTeacherListView;
+
+    @FXML
+    public void onLogoutButtonClickSwitchToLoginScene(ActionEvent event) throws IOException {
+        SceneSwitchService.getInstance().switchToLoginView(event);
+    }
+
+    @FXML
+    public void onNewCourseButtonClickSwitchToCourseDetailsScene(ActionEvent event) throws IOException{
+        SceneSwitchService.getInstance().switchToLoginView(event);
+    }
 }
