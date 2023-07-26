ec7b32ed3c003441c22df76d0c7d2db9924de278
Ziyang Song <>
2023-07-26 14:46:01 +0200

    Fix module info to make object mapper works

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -4,9 +4,18 @@ module de.iav.frontend {
     requires com.fasterxml.jackson.databind;
     requires java.net.http;
 
-
-    opens de.iav.frontend to javafx.fxml;
-    opens de.iav.frontend.controller to javafx.fxml;
+    opens de.iav.frontend;
     exports de.iav.frontend;
+
+    opens de.iav.frontend.model;
+    exports de.iav.frontend.model;
+
+    opens de.iav.frontend.controller;
     exports de.iav.frontend.controller;
+
+    opens de.iav.frontend.service;
+    exports de.iav.frontend.service;
+
+    opens de.iav.frontend.security;
+    exports de.iav.frontend.security;
 }
